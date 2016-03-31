12dba442be9eb3d6509858ef4cb299a5a5d6dde3
Darry Danzig <darry.d@gmail.com>
2016-03-30 18:18:01 -0700

    getCompetition method

--- app/src/main/java/com/darrydanzig/myfirstapp/activities/MyActivity.java
+++ app/src/main/java/com/darrydanzig/myfirstapp/activities/MyActivity.java
@@ -75,6 +75,7 @@ public class MyActivity extends AppCompatActivity {
         Log.e(TAG, "Total (without max and min) = " + judges.getTotal());
 
 
+        getCompetition(2);
 
     }
 
@@ -169,6 +170,29 @@ public class MyActivity extends AppCompatActivity {
                 });
 
 
+            }
+        });
+
+
+    }
+
+
+    private void getCompetition(int id) {
+
+        AsyncHttpGet url = new AsyncHttpGet(getString(R.string.BASE_URL)+ "/competition/show?json&id=" + id);
+
+        AsyncHttpClient.getDefaultInstance().executeJSONObject(url, new AsyncHttpClient.JSONObjectCallback() {
+            @Override
+            public void onCompleted(Exception e, AsyncHttpResponse source, JSONObject result) {
+                if (e != null) {
+                    e.printStackTrace();
+                    Log.e(TAG, "Exeception onCompleted. " + e.getMessage());
+                    return;
+                }
+
+
+                Log.e(TAG, "Competition json:");
+                Log.e(TAG, result.toString());
             }
         });
 
