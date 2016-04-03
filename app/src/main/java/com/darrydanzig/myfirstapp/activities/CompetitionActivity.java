157cecacc5ba16a51c5989e8b1469a52389fbcf0
Chris <disenchanted@live.ca>
2016-04-02 18:27:14 -0700

    Events

--- app/src/main/java/com/darrydanzig/myfirstapp/activities/CompetitionActivity.java
+++ app/src/main/java/com/darrydanzig/myfirstapp/activities/CompetitionActivity.java
@@ -2,27 +2,29 @@ package com.darrydanzig.myfirstapp.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.darrydanzig.myfirstapp.R;
+import com.darrydanzig.myfirstapp.adapter.PerformanceRenderer;
+import com.darrydanzig.myfirstapp.adapter.RoundRenderer;
+import com.darrydanzig.myfirstapp.models.Competition;
+import com.darrydanzig.myfirstapp.models.Performance;
 import com.darrydanzig.myfirstapp.models.Round;
-import com.darrydanzig.myfirstapp.adapter.RoundAdapter;
-import com.koushikdutta.async.http.AsyncHttpClient;
-import com.koushikdutta.async.http.AsyncHttpGet;
-import com.koushikdutta.async.http.AsyncHttpResponse;
+import com.pedrogomez.renderers.RendererAdapter;
+import com.pedrogomez.renderers.RendererBuilder;
 
-import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-/**
- * Created by Admin on 3/30/2016.
- */
-public class CompetitionActivity extends Activity {
+public class CompetitionActivity extends AppCompatActivity {
 
     @Bind(R.id.toolbar)
     Toolbar toolbar;
@@ -36,22 +38,31 @@ public class CompetitionActivity extends Activity {
         setContentView(R.layout.activity_my);
         ButterKnife.bind(this);
 
+        if( getIntent().getExtras() != null ) {
+            Competition competition = (Competition) getIntent().getExtras().getSerializable("competition");
 
-        Round[] rounds = new Round[10];
-        for (int i = 0; i < rounds.length; i++) {
-            rounds[i] = new Round(i);
+            toolbar.setTitle(competition.getTitle());
+
+            ArrayList<Round> rounds = new ArrayList<>();
+            Collections.addAll(rounds, competition.rounds );
+            Collections.sort(rounds);
+
+
+            list.setLayoutManager( new LinearLayoutManager( this ) );
+
+            RendererBuilder rendererBuilder = new RendererBuilder()
+                    .bind(Round.class, new RoundRenderer())
+                    .bind(Performance.class, new PerformanceRenderer());
+
+            RendererAdapter adapter = new RendererAdapter(rendererBuilder);
+            list.setAdapter(adapter);
+
+            for (Round round : rounds) {
+                adapter.add(round);
+                for (Map.Entry<Integer, Performance> integerPerformanceEntry : round.performances.entrySet()) {
+                    adapter.add(integerPerformanceEntry.getValue());
+                }
+            }
         }
-
-
-
-        list.setLayoutManager( new LinearLayoutManager( this ) );
-        RoundAdapter arrayAdapter = new RoundAdapter( CompetitionActivity.this, rounds );
-        list.setAdapter( arrayAdapter );
-
-
-        // HOW DO I GET THE COMPETITION CLASS PASSED INTO HERE?
-        //Log.e(TAG, "id = " + this.id);
     }
-
-
 }
